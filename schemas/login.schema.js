a487c06b94514981c905f766f7fa51ebde4fd62d
indianpojken <io.qrdr@gmail.com>
2023-04-06 08:52:24 +0200

    remove alphanum

--- schemas/login.schema.js
+++ schemas/login.schema.js
@@ -2,7 +2,7 @@ const joi = require('joi')
 
 const schema = joi.object({
   username: joi.string().alphanum().required(),
-  password: joi.string().alphanum().required()
+  password: joi.string().required()
 })
 
 module.exports = { loginSchema: schema }
--- schemas/signup.schema.js
+++ schemas/signup.schema.js
@@ -3,7 +3,7 @@ const joi = require('joi')
 const schema = joi.object({
   username: joi.string().alphanum().required(),
   email: joi.string().email().required(),
-  password: joi.string().alphanum().required()
+  password: joi.string().required()
 })
 
 module.exports = { signupSchema: schema }
